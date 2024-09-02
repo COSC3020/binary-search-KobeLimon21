732df110cf1c96f5e62eddda860e8f1222e144a7
KobeLimon21 <kobelimon2121@gmail.com>
2024-09-02 15:21:52 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
-function binarySearch(list, element) {
-    return -1;
-}
+// if array is empty return -1 
+// if array has elements, find middle element and if its middle return middle 
+// if not middle, binary search left half the array 
+
+
+function binarySearch(arr, target) {
+    let leftIndex = 0;
+    let rightIndex  = arr.length - 1;
+    
+   // finds middle index by adding left and right index together and dividing by 2 
+    while (leftIndex <= rightIndex){
+    const middleIndex = Math.floor((leftIndex + rightIndex) /2); 
+    
+    if(target === arr[middleIndex]) {
+    return middleIndex;
+    }
+     else if (target > arr[middleIndex]){
+    leftIndex = middleIndex + 1;
+    }
+    else {
+    rightIndex = middleIndex - 1;
+    }
+    }
+    return -1; 
+    }
+    
+    
