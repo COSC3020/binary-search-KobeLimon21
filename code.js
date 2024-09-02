4b25550029eafb632dcfc0757f3e55813566d937
KobeLimon21 <kobelimon2121@gmail.com>
2024-09-02 16:40:15 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 // if array is empty return -1 
 // if array has elements, find middle element and if its middle return middle 
-// if not middle, binary search left half the array 
+// if not middle, binary search left half the array or right depending on if its greater or less than
 
 
 function binarySearch(arr, target) {
     let leftIndex = 0;
     let rightIndex  = arr.length - 1;
-    let answer = -1; // sets result to -1 automatically unless "target" is found 
+    let answer = -1; // sets result to -1 automatically unless "target" is found (inputs that aren't found)
    
     while (leftIndex <= rightIndex){
     const middleIndex = Math.floor((leftIndex + rightIndex) /2); 
@@ -25,5 +25,8 @@ function binarySearch(arr, target) {
     }
     return answer; 
     }
-    
+
+
+// console.log(binarySearch([5,5,5,5] , 5)) for  duplicate number case test 
+// 
     
